93c05e4c1f1e0141ecc75979ef12f4774d4b7122
Erandir Mota <erandirsm13@gmail.com>
2023-07-23 23:36:39 -0300

    novo exercício

--- funcoes/aumentoSalario.js
+++ funcoes/aumentoSalario.js
@@ -1,27 +1,41 @@
-let nome;
-let salario;
-
 function perguntarDados() {
-  nome = prompt("Digite o nome do colaborador");
-  salario = parseFloat(prompt("Digite o salário do colaborador: "));
-  return calcularAumento(nome, salario);
+  let nome = prompt("Digite o nome do colaborador: ")
+  let salario = parseFloat(prompt("Digite o salário do colaborador: "))
+  calcularAumento(nome, salario)
 }
 
-let aumento = 0;
 function calcularAumento(nome, salario) {
+  let aumento = 0
+
   if (salario <= 1500) {
-    aumento = salario * 1.20;
+    aumento = 0.2
   } else if (salario <= 2000) {
-    aumento = salario * 1.15;
+    aumento = 0.15
   } else if (salario <= 3000) {
-    aumento = salario * 1.10;
+    aumento = 0.1
   } else {
-    aumento = salario * 0.5;
+    aumento = 0.05
   }
 
-  console.log(aumento);
+  let novoSalario = salario + salario * aumento
+
+  console.log(`Nome do colaborador: ${nome}`)
+  console.log(`Salário: R$ ${salario}`)
+  console.log(`Aumento: ${aumento * 100}%`)
+  console.log(`Salário reajustado: R$ ${novoSalario}`)
+
+  perguntarNovamente() // Perguntar se deseja continuar calculando
 }
+
+function perguntarNovamente() {
+  let resposta = prompt("Deseja calcular novamente? (s) sim ou (n) não: ")
+
+  if (resposta.toLowerCase() === "s") {
+    perguntarDados() // Reiniciar o processo
+  } else {
+    console.log("Programa encerrado.")
+  }
+}
+
+// Início do programa
 perguntarDados()
-calcularAumento()
-
-
