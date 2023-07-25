478648f075e7bfd38bd2f410b4cb7d1488bbb354
Erandir Mota <erandirsm13@gmail.com>
2023-07-25 15:44:51 -0300

    atualizando exercicio

--- funcoes/aumentoSalario.js
+++ funcoes/aumentoSalario.js
@@ -1,41 +1,46 @@
 function perguntarDados() {
-  let nome = prompt("Digite o nome do colaborador: ")
-  let salario = parseFloat(prompt("Digite o salário do colaborador: "))
-  calcularAumento(nome, salario)
+  let nome = prompt("Digite o nome do colaborador: ");
+  let salario = parseFloat(prompt("Digite o salário do colaborador: "));
+  calcularAumento(nome, salario);
 }
 
 function calcularAumento(nome, salario) {
-  let aumento = 0
+  let aumento = 0;
 
   if (salario <= 1500) {
-    aumento = 0.2
+    aumento = 0.2;
   } else if (salario <= 2000) {
-    aumento = 0.15
+    aumento = 0.15;
   } else if (salario <= 3000) {
-    aumento = 0.1
+    aumento = 0.1;
   } else {
-    aumento = 0.05
+    aumento = 0.05;
   }
 
-  let novoSalario = salario + salario * aumento
+  let novoSalario = salario + (salario * aumento);
 
-  console.log(`Nome do colaborador: ${nome}`)
-  console.log(`Salário: R$ ${salario}`)
-  console.log(`Aumento: ${aumento * 100}%`)
-  console.log(`Salário reajustado: R$ ${novoSalario}`)
+  // Exibir resultados
+  console.log(`Nome do colaborador: ${nome}`);
+  console.log(`Salário R$ ${salario}`);
+  console.log(`Aumento: ${aumento * 100}%`);
+  console.log(`Salário reajustado: R$ ${novoSalario}`);
 
-  perguntarNovamente() // Perguntar se deseja continuar calculando
+  perguntarNovamente(); // Perguntar se deseja calcular de novo
 }
 
+// Função que pergunta ao usuário se ele deseja calcular novamente
 function perguntarNovamente() {
-  let resposta = prompt("Deseja calcular novamente? (s) sim ou (n) não: ")
+  let resposta = prompt("Deseja calcular novamente? (s) Sim ou (n) Não: ");
 
-  if (resposta.toLowerCase() === "s") {
-    perguntarDados() // Reiniciar o processo
+  if (resposta == "s") {
+    perguntarDados(); // Volta o processo
   } else {
-    console.log("Programa encerrado.")
+    console.log("Programa encerrado!");
   }
 }
 
-// Início do programa
-perguntarDados()
+// Inicio do programa
+perguntarDados();
+
+
+
