99a3582b8dae1d72c7c0ee707d479b10dfde468b
Erandir Mota <erandirsm13@gmail.com>
2023-08-01 21:28:10 -0300

    atualizando exercicio

--- funcoes/loja.js
+++ funcoes/loja.js
@@ -1,15 +1,34 @@
 let id = [];
-let produto = [];
+let nome = [];
 let preco = [];
+let avaliacao = [];
 
-function procurarPorId() {
+function cadastrarProduto() {
+  let contador = 0;
 
+  let cadastrarID = parseInt(prompt("Digite o ID do produto:"));
+  id[contador] = cadastrarID;
+
+  let cadastrarNome = prompt("Digite o nome do produto: ");
+  nome[contador] = cadastrarNome;
+
+  let cadastrarPreco = parseFloat(prompt("Digite o preço do produto: "));
+  preco[contador] = cadastrarPreco;
+
+  let cadastrarAvaliacao = parseFloat(prompt("Digite a avaliação do produto: "));
+  avaliacao[contador] = cadastrarAvaliacao;
+
+  return { id, nome, preco, avaliacao };
 }
 
-function procurarPorNome() {
+const dadosRetornados = cadastrarProduto(); 
 
-}]
-
-function procurarPorPreco() {
-
+function buscarPorID() {
+  console.log(dadosRetornados.id);
+  console.log(dadosRetornados.nome);
 }
+
+cadastrarProduto();
+buscarPorID();
+
+
