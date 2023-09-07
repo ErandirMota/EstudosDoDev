0c81d5ac23185063de71f854e86067b15614a9ec
Erandir Mota <erandirsm13@gmail.com>
2023-09-06 23:12:45 -0300

    revisando exercicio

--- funcoes_de_arrays/20-funcoesArray.js
+++ funcoes_de_arrays/20-funcoesArray.js
@@ -1,65 +1,60 @@
 let nomes = [];
 let senhas = [];
 
+// Usuário vai escolher o que deseja fazer
 function desejaFazer() {
-  let opcao = parseInt(
-    prompt(
-      "Escolha: \n1 - Cadastrar \n2 - Fazer login \n3 - Excluir cadastro \n4 - Encerrar programa"
-    )
-  );
-  return opcao;
+  let escolha = parseInt(prompt("\n1 - Cadastrar \n2 - Fazer login \n3- Excluir cadastro \n4- Encerrar programa"));
+  return escolha;
 }
 
+// Cadastrar um novo usuário
 function cadastrar() {
-  let nome = prompt("Digite o nome que deseja cadastrar: ");
-  let senha = prompt("Digite a senha que deseja cadastrar: ");
+  let nome = prompt("Insira um nome: ");
+  let senha = prompt("Insira uma senha: ");
+
   nomes.push(nome);
   senhas.push(senha);
-  console.log("Cadastro realizado com sucesso!");
 }
 
+// Verificar se login tem nome e senha corretas
 function fazerLogin(nome, senha) {
   let indice = nomes.indexOf(nome);
-  if (indice !== -1 && senhas[indice] === senha) {
+  if (indice !== -1 && senhas[indice] == senha) {
     return true;
   } else {
     return false;
   }
 }
 
-function excluirCadastro(nomeExcluir) {
-  let indice = nomes.indexOf(nomeExcluir);
+// EXcluir um cadastro de usuário
+function excluirCadastro(excluirNome) {
+  let indice = nomes.indexOf(excluirNome);
   if (indice !== -1) {
     nomes.splice(indice, 1);
     senhas.splice(indice, 1);
     console.log("Cadastro excluído com sucesso!");
   } else {
-    console.log("Usuário não foi encontrado!");
+    console.log("Usuário não encontrado");
   }
 }
 
-// Fluxo de funcionamento
+// Fluxo de funcionamento do programa
 let continuar = true;
 while (continuar) {
-  let opcao = desejaFazer();
+  let escolha = desejaFazer();
 
-  switch (opcao) {
+  switch (escolha) {
     case 1:
       cadastrar();
       break;
     case 2:
-      let nomeLogin = prompt("Digite o nome de login: ");
-      let senhaLogin = prompt("Digite a senha de login: ");
-      let login = fazerLogin(nomeLogin, senhaLogin);
-      if (login) {
-        console.log("Login feito com sucesso!");
-      } else {
-        console.log("Nome ou senha estão incorretos!")
-      };
+      let nome = prompt("Digite o nome do usuário: ");
+      let senha = prompt("Digite a senha do usuário: ");
+      fazerLogin(nome, senha);
       break;
     case 3:
-      let nomeExcluir = prompt("Digite o nome que deseja excluir: ");
-      excluirCadastro(nomeExcluir);
+      let excluirNome = prompt("Digite o nome de usuário que deseja excluir: ");
+      excluirCadastro(excluirNome);
       break;
     case 4:
       console.log("Programa encerrado.");
