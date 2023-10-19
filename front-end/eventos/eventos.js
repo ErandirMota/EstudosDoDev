fcd78296fa55cf8d67aeb438fd443e831e14903f
Erandir Mota <erandirsm13@gmail.com>
2023-10-18 23:33:41 -0300

    estudando sobre eventos

--- front-end/eventos/eventos.js
+++ front-end/eventos/eventos.js
@@ -5,3 +5,24 @@ function performAction(event) {
     console.log("Olá")
     console.log(event)
 }
+
+document.getElementById("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(event)
+    alert("Recebi o evento")
+})
+
+const btnEmail = document.getElementById("testeEmail")
+
+btnEmail.addEventListener("focus", (event) => {
+    console.log("Focou no elemento")
+  })
+
+btnEmail.addEventListener("blur", (event) => {
+  console.log("Tirou o foco do elemento")
+})
+
+document.addEventListener("scroll", (event) => {
+    console.log("escrollou né!")
+    console.log(event)
+})
