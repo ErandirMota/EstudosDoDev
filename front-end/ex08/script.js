ad3e13b680d400eac7bbbdf7d36fb9e697b8c9df
Erandir Mota <erandirsm13@gmail.com>
2023-10-26 16:33:05 -0300

    testando exercicio

--- front-end/ex08/script.js
+++ front-end/ex08/script.js
@@ -1,1 +1,14 @@
-document 
+const name = document.getElementById("textName");
+const address = document.getElementById("textAddress");
+const hour = document.getElementById("textHour");
+
+name.addEventListener("focus", (event) =>  {
+  event.target.style.border = purple;
+})
+address.addEventListener("blur", (event) =>  {
+  console.log("Desfocou do elemento")
+})
+
+hour.addEventListener("blur", (event) => {
+  console.log("Desfocou do elemento")
+})
