b4ab909a045723d302a76865209c16a9aa0eb22b
unknown <erandirsm13@gmail.com>
2023-11-19 21:35:42 -0300

    exercise about inheritance and methods.

--- orientacao_objetos/exercicios/exHeranca.js
+++ orientacao_objetos/exercicios/exHeranca.js
@@ -1,40 +1,83 @@
 class Employee {
-  name
-  salary
-
+  constructor(name, salary) {
+    this.Name = name
+    this.Salary = salary
+  }
 }
 
 class Manager extends Employee {
-  department
-
   constructor(name, salary, department) {
-    super();
-    this.name = name;
-    this.salary = salary;
-    this.department = department;
+    super(name, salary)
+    this.Department = department
   }
 
-  displayInformation(name, salary, department) {
-    console.log(name, salary, department);
+  DisplayInformations() {
+    console.log(
+      `Name: ${this.Name}, Salary: ${this.Salary}, Department: ${this.Department}`
+    )
   }
 }
 
 class Seller extends Employee {
-  comissionPercentage
-
-  constructor(comissionPercentage, name, salary) {
-    super();
-    this.comissionPercentage = comissionPercentage;
-    this.name = name;
-    this.salary = salary;
+  constructor(name, salary, commissionPercentage) {
+    super(name, salary)
+    this.CommissionPercentage = commissionPercentage
   }
 
-  calculateSalary(salary, comissionPercentage) {
-    salaryWithComission = salary + comissionPercentage;
-    return salaryWithComission;
+  CalculateSalary() {
+    return this.Salary + this.Salary * (this.CommissionPercentage / 100)
   }
 
-  displayInformation(name, salary, salaryWithComission, comissionPercentage) {
-    console.log(name, salary, salaryWithComission, comissionPercentage);
+  DisplayInformations() {
+    const salaryWithCommission = this.CalculateSalary()
+    console.log(
+      `Name: ${this.Name}, Salary: ${this.Salary}, Salary with commission: ${salaryWithCommission}, Commission Percentage: ${this.CommissionPercentage}%`
+    )
   }
 }
+
+class Product {
+  constructor(name, value) {
+    this.Name = name
+    this.Value = value
+  }
+}
+
+class Sale {
+  constructor(seller) {
+    this.Seller = seller
+    this.ProductList = []
+    this.TotalValue = 0
+  }
+
+  AddProduct(name, value) {
+    const newProduct = new Product(name, value)
+    this.ProductList.push(newProduct)
+  }
+
+  CalculateTotal() {
+    this.TotalValue = this.ProductList.reduce(
+      (total, product) => total + product.Value,
+      0
+    )
+  }
+
+  FinalizeSale() {
+    this.CalculateTotal()
+    console.log(
+      `Finalized sale - Seller: ${this.Seller.Name}, Total value: ${this.TotalValue}`
+    )
+  }
+}
+
+let employee = new Employee("João", 5600)
+let manager = new Manager("Erandir", 15000, "Technology")
+
+const seller = new Seller("Alice", 3000, 5)
+const sale = new Sale(seller)
+sale.AddProduct("Mouse", 234.98)
+sale.AddProduct("Keyboard", 70.98)
+sale.FinalizeSale()
+
+manager.DisplayInformations()
+seller.DisplayInformations()
