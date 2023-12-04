0b00aa1baf24be99742cfd3624b19fc8c1eeecd3
unknown <erandirsm13@gmail.com>
2023-12-04 00:30:09 -0300

    exercise finalized

--- orientacao_objetos/exercicios/exImovel-28.js
+++ orientacao_objetos/exercicios/exImovel-28.js
@@ -2,7 +2,7 @@ class Immobile {
   Address
   ZipCode
   AreaInMeters
-  BathroomsNumber
+  #BathroomsNumber
   RoomsNumber
   PeopleLiving
   ImmobileValue
@@ -15,6 +15,14 @@ class Immobile {
     this.RoomsNumber = roomsNumber;
     this.PeopleLiving = peopleLiving;
     this.ImmobileValue = immobileValue;
+  }
+
+  addPerson(person) {
+    this.PeopleLiving.push(person);
+  }
+
+  removePerson(person) {
+    this.PeopleLiving = this.PeopleLiving.filter(p => p !== person);
   }
 }
 
@@ -31,30 +39,53 @@ class Person {
 }
 
 class House extends Immobile {
-  constructor(withYard) {
-    this.withYard = true;
+  constructor(address, zipCode, areaInMeters, bathroomsNumber, roomsNumber, peopleLiving, immobileValue, withYard) {
+    super(address, zipCode, areaInMeters, bathroomsNumber, roomsNumber, peopleLiving, immobileValue)
+    this.withYard = withYard;
+  }
+
+  CalculateValue() {
+    if (this.withYard) {
+      return this.ImmobileValue += this.ImmobileValue * 0.30;
+    } else {
+      return this.ImmobileValue
+    }
   }
 }
 
 class Apartment extends Immobile {
-  ApartmentFloor
-  CondominiumValue
-
-  constructor(apartmentFloor, condominiumValue) {
+  constructor(address, zipCode, areaInMeters, bathroomsNumber, roomsNumber, peopleLiving, immobileValue, apartmentFloor, condominiumValue) {
+    super(address, zipCode, areaInMeters, bathroomsNumber, roomsNumber, peopleLiving, immobileValue);
     this.ApartmentFloor = apartmentFloor;
     this.CondominiumValue = condominiumValue;
   }
 
-  AddPeople() {
-
+  AddPeople(person) {
+    this.PeopleLiving.push(person)
   }
 
-  RemovePeople() {
-
+  RemovePeople(person) {
+    this.PeopleLiving = this.PeopleLiving.filter((p) => p !== person)
+  }
+  CalculateValue() {
+    this.ImmobileValue += this.CondominiumValue
+    return this.ImmobileValue
   }
 }
 
 class Kitnet extends Immobile {
-  
+  constructor(address, zipCode, areaInMeters, bathroomsNumber, roomsNumber, peopleLiving, immobileValue) {
+    super(address, zipCode, areaInMeters, bathroomsNumber, roomsNumber, peopleLiving, immobileValue);
+  }
+
+  CalculateValue() {
+    return this.ImmobileValue;
+  }
 }
+
+const person1 = new Person("John Doe", "123456789", "01/01/1990");
+const house = new House("Rua A", "12345-678", 100, 2, 3, [person1], 200000, true);
+house.addPerson(new Person("Jane Doe", "987654321", "02/02/1995"));
+console.log(house.CalculateValue());
+console.log(house.PeopleLiving);
 
